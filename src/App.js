f403368a670a1df4e6ce33c84182b89e44be6ff9
Kevin Haskett <kevin.haskett@siriuscom.com>
2020-01-22 15:27:45 -0600

    adding header component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 import HomePage from './pages/homepage'
 import ShopPage from './pages/shopPage'
+import Header from './components/header'
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
