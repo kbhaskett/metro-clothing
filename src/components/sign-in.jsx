c689cf384a4b575d67d65f49368489b1c42d5a65
Kevin Haskett <kevin.haskett@siriuscom.com>
2020-01-24 15:53:00 -0600

    finish signup

--- src/components/sign-in.jsx
+++ src/components/sign-in.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import FormInput from './form-input'
 import CustomButton from './custom-button'
 
-import {signInWithGoogle} from '../firebase/firebase-utils'
+import {auth, signInWithGoogle} from '../firebase/firebase-utils'
 
 import './sign-in.styles.scss'
 
@@ -15,9 +15,15 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email: '', password: ''});
+        const {email, password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''});
+        } catch(err) {
+            console.log(err);
+        }
     }
 
     handleChange = event => {
