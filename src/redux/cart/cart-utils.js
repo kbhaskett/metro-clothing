983ef371c746e53833e1df797d8ca09042beb960
Kevin Haskett <kevin.haskett@siriuscom.com>
2020-01-29 11:05:25 -0600

    added quantity tracking

---
+++ src/redux/cart/cart-utils.js
@@ -0,0 +1,14 @@
+export const addItemToCart = (cart, newItem) => {
+    const existingItem = cart.find(
+        item => item.id === newItem.id
+    )
+    if (existingItem) {
+        return cart.map(
+            item => item.id === newItem.id
+            ? {...item, quantity: item.quantity + 1}
+            : item 
+        )
+    }
+
+    return [...cart, {...newItem, quantity: 1}]
+}
