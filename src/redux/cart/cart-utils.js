a8cbe1869c134237819a1531e29476ac41ae1cc3
Kevin Haskett <kevin.haskett@siriuscom.com>
2020-01-30 14:45:20 -0600

    added addItem and removeItem functionality

--- src/redux/cart/cart-utils.js
+++ src/redux/cart/cart-utils.js
@@ -13,6 +13,25 @@ export const addItemToCart = (cart, newItem) => {
     return [...cart, {...newItem, quantity: 1}]
 }
 
+export const removeItemFromCart = (cart, itemToRemove) => {
+    const existingItem = cart.find(
+        item => item.id === itemToRemove.id
+    )
+    if (existingItem) {
+        if (existingItem.quantity === 1) {
+            return cart.filter(cartItem => cartItem.id !== itemToRemove.id);
+        } else {
+            return cart.map(
+            item => item.id === itemToRemove.id
+            ? {...item, quantity: item.quantity - 1 }
+            : item 
+            )
+        }
+    }
+
+    return [...cart]
+}
+
 export const clearItemfromCart = (cart, itemToClear) => {
     return cart.filter(cartItem => cartItem.id !== itemToClear.id);
 }
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -9,6 +9,11 @@ export const addItem = item => ({
     payload: item
 })
 
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+})
+
 export const clearItemfromCart = item => ({
     type: CartActionTypes.CLEAR_CART_ITEM,
     payload: item
