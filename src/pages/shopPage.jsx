94131eed064626ef9ada2de885d34b23b77f7908
Kevin Haskett <kevin.haskett@siriuscom.com>
2020-01-31 14:42:44 -0600

    collections redux

--- src/pages/shopPage.jsx
+++ src/pages/shopPage.jsx
@@ -1,24 +1,23 @@
-import React, {Component} from 'react';
-import CollectionPreview from '../components/collection-preview';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import SHOP_DATA from './shop.data';
+import { selectCollections } from "../redux/shop/shop.selector";
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render() {
-        const {collections} = this.state;
-        return (<div className='shop-page'>
-            {collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))}
-        </div>
-        )
-    }
-}
+import CollectionPreview from "../components/collection-preview";
 
-export default ShopPage
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+const mapPropsToDispatch = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapPropsToDispatch)(ShopPage);
